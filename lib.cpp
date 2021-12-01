a3b87046d72bb7742cce0e4078444a0deafd54a0
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-12-01 19:02:46 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include "lib.h"
+bool numerip (int n,int a){
+  if(n<0){
+    return false;
+  }else if(n==1){
+    return true;
+  }else if(a==1){
+    return true;
+  }else if(n%a==0){
+    return false;
+  }
+  numerip(n,--a);
+}
